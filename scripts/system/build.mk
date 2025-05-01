50a6653eaed3f8a07924a9ec47cd27036ab7e3b6
Vyacheslav Tievsky <>
2025-05-01 13:10:31 +0300

    fix docker compose

--- scripts/system/build.mk
+++ scripts/system/build.mk
@@ -1,6 +1,13 @@
 APP=auth-id
 APP_TAG=slaventius/${APP}:latest
+# APP_TAG=slaventius/${APP}:v$(shell date +%s)
 
 docker-build:
 	@echo "building docker-image ${APP_TAG}"
 	@docker build --no-cache --tag ${APP_TAG} --file build/app/Dockerfile .
+
+up:  ## Поднять и запустить зависимости в контейнерах
+	docker compose -f build/docker-compose.yml up -d --build
+
+down:  ## Остановить и удалить зависимости
+	docker compose -f build/docker-compose.yml down --remove-orphans
